484b2a1102270361b01e57780d7d1b8f08cc09d0
Mohammed-Alsenani2 <Mohammed.Mosa.eam.js@tuwaiq.edu.sa>
2021-11-21 16:46:29 +0300

    Upload Data

---
+++ Data/Promessions.js
@@ -0,0 +1,34 @@
+const permissions = [
+  {
+    userId: "1010101010",
+    permission: [
+      {
+        type: "",
+        place: "",
+        numberOfPeople: "",
+      },
+    ],
+  },
+  {
+    userId: "1111111111",
+    permission: [
+      {
+        type: "",
+        place: "",
+        numberOfPeople: "",
+      },
+    ],
+  },
+  {
+    userId: "0000000000",
+    permission: [
+      {
+        type: "",
+        place: "",
+        numberOfPeople: "",
+      },
+    ],
+  },
+];
+
+module.exports = { permissions };
