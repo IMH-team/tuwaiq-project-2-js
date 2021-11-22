1cbef2edc01c0467e684090b4e451ce2f32905b6
hamadsr <hamad.sarrar.eam.js@tuwaiq.edu.sa>
2021-11-22 11:52:23 +0300

    Nav Bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Navigation from './components/navigation/index'
+
 require("dotenv").config();
 
 const App = () => {
-	return <div className="App">App component</div>;
+	return <div className="App">
+		<Navigation/></div>;
 };
 
 export default App;
