ab5ae5545e6f9d7f2cd0cc8eaed7fa66ade1596e
Mohammed-Alsenani2 <Mohammed.Mosa.eam.js@tuwaiq.edu.sa>
2021-11-23 16:00:46 +0300

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import Navigation from './components/navigation/index'
+import React from "react";
+import { Route } from "react-router-dom";
+import Navigation from "./components/navigation/index";
+import Dashboard from "./components/dashboard/index";
 
 require("dotenv").config();
 
 const App = () => {
-	return <div className="App">
-		<Navigation/></div>;
+  return (
+    <div className="App">
+      <Navigation />
+      <Dashboard />
+    </div>
+  );
 };
 
 export default App;
--- frontend/src/components/dashboard/index.js
+++ frontend/src/components/dashboard/index.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import MyCard from "./MyCard";
+
+function getData() {
+  fetch("/users")
+    .then((res) => res.json())
+    .then((data) => {
+      const info = data.find((user) => user.userId === "1010101010");
+      console.log(info.userInfo);
+    })
+    .catch();
+}
 
 const Dashboard = () => {
-	return <div className="App">Dashboard component</div>;
+  return (
+    <div className="App">
+      <Container>
+        <Row md={4}>
+          <Button onClick={getData}></Button>
+          <MyCard />
+          <Col>2 of 3</Col>
+          <Col>3 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Dashboard;
