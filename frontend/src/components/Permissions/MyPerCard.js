90b77a2104589a7195dc421968eb0fc83d63aafb
IbrahimNAlyami <Ibrahim.nashir.eam.js@tuwaiq.edu.sa>
2021-11-26 15:29:26 +0300

    updatedMyPerCard

--- frontend/src/components/Permissions/MyPerCard.js
+++ frontend/src/components/Permissions/MyPerCard.js
@@ -1,17 +1,20 @@
 import React from "react";
-import { Col, Card } from "react-bootstrap";
+import { Col, Card, Container } from "react-bootstrap";
 // import "./MyCard.css";
 
 export default function MyPerCard(props) {
   return (
+    <Container>
     <Col>
-      <Card className="myCard">
+      <div className="myCard" style={{ textAlign: "center",  marginLeft: "auto",
+                marginRight: "auto", width: "50%"}}>
         <Card.Body style={{ textAlign: "center" }}>
           <Card.Title>{props.type}</Card.Title>
           <Card.Text>{props.place}</Card.Text>
           <Card.Text>{props.numberOfPeople}</Card.Text>
         </Card.Body>
-      </Card>
+      </div>
     </Col>
+    </Container>
   );
 }
